4f7cf7856560d9853753a9ab52bf5f64180ea918
Eugen Mirce <eugen.mirce@lhind.dlh.de>
2025-01-24 12:17:21 +0100

    Fix CORS

--- Backend/src/main/java/com/klod/inventory_managment_system/config/security/SecurityConfig.java
+++ Backend/src/main/java/com/klod/inventory_managment_system/config/security/SecurityConfig.java
@@ -16,13 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig implements WebMvcConfigurer {
+public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
     private final JWTAuthFilter jwtAuthFilter;
@@ -30,7 +28,6 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                //.cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated())
@@ -49,13 +46,5 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Allow frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow methods
-                .allowedHeaders("*") // Allow headers
-                .allowCredentials(true); // Allow credentials (cookies)
     }
 }
